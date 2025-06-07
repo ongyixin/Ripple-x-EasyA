069410db596f1e77d1855a7857bdc1e4f8be3d28
Ong Yi Xin <yixinong34@gmail.com>
2025-06-08 00:39:03 +0800

    Initial commit with local files

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import json
-import os
-from datetime import datetime
-import mod1  # XRPL wallet functions
-import mod2  # Token/currency functions
 from cli_handlers import cli_handle
 
 def main():
